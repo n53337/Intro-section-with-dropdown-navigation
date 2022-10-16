3f6853ee8e85d9884b3f018f673d6d0810e6bcbe
u53f <scalewithusef@gmail.com>
2022-10-16 13:00:49 +0100

    finish the header

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,17 @@
 const nav = document.querySelector(".header__nav");
 const navIcon = document.querySelector(".menu-btn>img");
 
+const subMenu = [
+  {
+    nav: document.querySelector(".nav-features"),
+    sub: document.querySelector(".sub-features"),
+  },
+  {
+    nav: document.querySelector(".nav-company"),
+    sub: document.querySelector(".sub-company"),
+  },
+];
+
 // navbar toggle
 navIcon.addEventListener("click", () => {
   nav.classList.toggle("nav-hide");
@@ -12,4 +23,11 @@ navIcon.addEventListener("click", () => {
   } else {
     navIcon.setAttribute("src", "./images/icon-close-menu.svg");
   }
+});
+
+// subMenu toggle
+subMenu.forEach((e, i) => {
+  e.nav.addEventListener("click", () => {
+    e.sub.classList.toggle("d-none");
+  });
 });
